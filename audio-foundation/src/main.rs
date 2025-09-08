b4b00a7a7fb0cd4a0d13b878b4b05be3580235a6
matthiasmeissen <meissenmatthias@gmail.com>
2025-09-08 13:45:10 +0200

    feat: Get basic audio device

--- audio-foundation/src/main.rs
+++ audio-foundation/src/main.rs
@@ -1,4 +1,26 @@
+use cpal::traits::{DeviceTrait, HostTrait};
+
 
 fn main() {
-    println!("Audio Foundation");
+    println!("Setting up audio pipeline.");
+
+    // Get the default audio host (provides access to available audio devices)
+    let host = cpal::default_host();
+    println!("Audio Host: {}", host.id().name());
+
+    // Find the default output device
+    let device = host.default_output_device().expect("No output device available.");
+    println!("Output device: {:?}", device.name());
+
+    // Get default stream configuration (Includes preferred sample rate and channel count)
+    let config = device.default_output_config().expect("No default output config found.");
+    println!("Default output config: {:#?}", config);
+
+    // Store sample format from config as variable to use it later
+    let sample_format = config.sample_format();
+    println!("Expected sample format: {}", sample_format);
+
+    // Get stream configuration struct
+    let stream_config: cpal::StreamConfig = config.into();
+    println!("Stream config: {:#?}", stream_config);
 }
