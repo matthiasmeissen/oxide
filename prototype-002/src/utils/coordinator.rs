3848621477fe909f02b40a1af7b2188e9584bfa9
matthiasmeissen <meissenmatthias@gmail.com>
2025-09-19 14:28:57 +0200

    Restore directory structure

---
+++ prototype-002/src/utils/coordinator.rs
@@ -0,0 +1,55 @@
+use crate::utils::state::*;
+
+use std::thread;
+use crossbeam_channel::Receiver;
+use triple_buffer::Input;
+
+const DEBUG: bool = false;
+
+pub fn start_coordinator_thread(
+    receiver: Receiver<Message>, 
+    mut window_writer: Input<State>, 
+    mut audio_writer: Input<State>) 
+    {
+    thread::spawn(move || {
+        let mut current_state = State::default();
+        let mut last_published_state = current_state;
+
+        while let Ok(update) = receiver.recv() {
+            match update {
+                Message::SetTime(t) => current_state.time = t,
+                Message::SetResolution(w, h) => current_state.resolution = [w, h],
+                Message::SetValue(i, v) => current_state.values[i] = v,
+                Message::MidiInput(midi) => match midi {
+                    MidiMessage::ControlChange { controller, value } => {
+                        if controller == 77 {
+                            let val = value as f64 / 128.0;
+                            current_state.values[0] = val;
+                        }
+                        if controller == 78 {
+                            let val = value as f64 / 128.0;
+                            current_state.values[1] = val;
+                        }
+                        if controller == 79 {
+                            let val = value as f64 / 128.0;
+                            current_state.values[2] = val;
+                        }
+                        if controller == 80 {
+                            let val = value as f64 / 128.0;
+                            current_state.values[3] = val;
+                        }
+                        println!("{:?}", current_state);
+                    }
+                    _ => ()
+                },
+            }
+
+            if current_state != last_published_state {
+                window_writer.write(current_state);
+                audio_writer.write(current_state);
+                last_published_state = current_state;
+                if DEBUG {println!("// -> Cooridnator published new state. {:#?}", current_state)} else {};
+            }
+        }
+    });
+}
