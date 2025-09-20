d0cd7cb2fe48435b48532bafd55d0c1b1a012d0f
matthiasmeissen <meissenmatthias@gmail.com>
2025-09-20 16:55:58 +0200

    PT1 - Adjust shader-02

--- prototype-001/src/shaders/shader-02.glsl
+++ prototype-001/src/shaders/shader-02.glsl
@@ -14,13 +14,27 @@ uniform float u_gate2;
 uniform float u_gate3;
 uniform float u_gate4;
 
+#define PI 3.14159265359
 #define rot(a) mat2(cos(a), -sin(a), sin(a), cos(a))
 
 void main() {
     vec2 uv = v_uv;
     vec2 p = (gl_FragCoord.xy * 2.0 - u_resolution.xy) / min(u_resolution.x, u_resolution.y);
 
-    float d = step(length(p), u_cv1);
+    float size = mix(0.4, 2.0, u_cv1);
+    float s1 = mix(2.0, 10.0, u_cv2);
+    float s2 = mix(4.0, 20.0, u_cv3);
+
+    vec2 p1 = p + size;
+    p1 = rot(u_time * 0.4) * p1;
+
+    vec2 p2 = rot(p1.x) * p;
+
+    float d = distance(p, vec2(cos(p2.x * s1), p.y)) * distance(p1, vec2(p.x, sin(p.y * s2)));
+
+    d = step(u_cv4, d);
+
+    d = mix(d, 1.0 - d, u_gate1);
 
     vec3 col = vec3(d);
 
