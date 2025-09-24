a61ea291aa947544d9dd5913671ddf82d05c9bb7
matthiasmeissen <meissenmatthias@gmail.com>
2025-09-24 18:54:25 +0200

    Clean up oled tests

--- embedded-foundation/src/main.rs
+++ embedded-foundation/src/main.rs
@@ -1,42 +1,29 @@
-use embedded_graphics::{
-    pixelcolor::BinaryColor,
-    prelude::*,
-    primitives::{Line, PrimitiveStyle},
-};
-use linux_embedded_hal::I2cdev;
 
-// --- THE FIX IS HERE ---
-// We must explicitly import `I2CDisplayInterface` from the `sh1106` crate.
-use sh1106::{prelude::*, I2CDisplayInterface, Sh1106};
+mod modules;
+use modules::{
+    display::*,
+    coordinator::*,
+    midi::*,
+};
+
+use crossbeam_channel;
+use triple_buffer::TripleBuffer;
+
+use crate::modules::state::PlaceholderState;
+
+// To run the simulator run this command in the terminal
+// export LIBRARY_PATH="$LIBRARY_PATH:$(brew --prefix)/lib"
+// This will enable it for that session only
+
 
 fn main() {
-    // I2C setup is exactly the same
-    let i2c = I2cdev::new("/dev/i2c-1").expect("Failed to open I2C device");
+    println!("Embedded Foundation");
+    let (sender, receiver) = crossbeam_channel::bounded(5);
+    let (display_writer, display_reader) = TripleBuffer::new(&PlaceholderState::new()).split();
 
-    // Now the compiler knows what `I2CDisplayInterface` is.
-    let interface = I2CDisplayInterface::new(i2c);
+    start_coordinator_thread(receiver, display_writer);
 
-    // Create an Sh1106 driver instance
-    let mut display = Sh1106::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
-        .into_buffered_graphics_mode();
+    start_midi_thread(sender.clone());
 
-    // The rest of the code remains the same
-    display.init().expect("Failed to initialize display");
-
-    display.clear(BinaryColor::Off).expect("Clear failed");
-
-    let line_style = PrimitiveStyle::with_stroke(BinaryColor::On, 1);
-    Line::new(Point::new(0, 0), Point::new(127, 63))
-        .into_styled(line_style)
-        .draw(&mut display)
-        .expect("Line draw failed");
-
-    display.flush().expect("Flush failed");
-
-    println!("Line should be visible on your SH1106 screen!");
-    println!("Program will now loop forever. Press Ctrl+C to exit.");
-
-    loop {
-        std::thread::sleep(std::time::Duration::from_millis(500));
-    }
+    start_display(display_reader).expect("Error starting display.");
 }
