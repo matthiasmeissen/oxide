b0fbff12bde16ec1abedcf8f4fdac52b3aff74fd
matthiasmeissen <meissenmatthias@gmail.com>
2025-10-02 17:57:07 +0200

    EMB - Add Pot input

--- embedded-foundation/src/modules/i2c.rs
+++ embedded-foundation/src/modules/i2c.rs
@@ -7,7 +7,7 @@ use std::thread;
 use std::time::Duration;
 use std::convert::TryInto;
 
-const STATE_SIZE: usize = 7;
+const STATE_SIZE: usize = 9;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 struct DeviceState {
@@ -15,6 +15,7 @@ struct DeviceState {
     button2: bool,
     encoder_button: bool,
     encoder_value: i32,
+    pot_value: i16,
 }
 
 pub fn start_i2c_thread(sender: Sender<Message>) {
@@ -35,16 +36,13 @@ pub fn start_i2c_thread(sender: Sender<Message>) {
                         button1: buffer[0] != 0,
                         button2: buffer[1] != 0,
                         encoder_button: buffer[2] != 0,
-                        encoder_value: {
-                            let bytes: [u8; 4] = buffer[3..7].try_into().unwrap_or_default();
-                            i32::from_le_bytes(bytes)
-                        },
+                        encoder_value: i32::from_le_bytes(buffer[3..7].try_into().unwrap_or_default()),
+                        pot_value: u16::from_le_bytes(buffer[7..9].try_into().unwrap_or_default()),
                     };
 
                     if let Some(prev_state) = last_state {
                         if current_state.encoder_value != prev_state.encoder_value {
-                            let normalized_value = normalize_encoder(current_state.encoder_value);
-                            sender.send(Message::SetValue(0, normalized_value)).unwrap();
+                            sender.send(Message::SetValue(0, normalize_enc(current_state.encoder_value))).unwrap();
                         }
 
                         if current_state.button1 != prev_state.button1 {
@@ -61,6 +59,10 @@ pub fn start_i2c_thread(sender: Sender<Message>) {
                             let value = if current_state.encoder_button { 1.0 } else { 0.0 };
                             sender.send(Message::SetValue(6, value)).unwrap();
                         }
+
+                        if current_state.pot_value != prev_state.pot_value {
+                            sender.send(Message::SetValue(1, normalize_pot(current_state.pot_value))).unwrap();
+                        }
                     }
 
                     last_state = Some(current_state);
@@ -75,9 +77,13 @@ pub fn start_i2c_thread(sender: Sender<Message>) {
     });
 }
 
-fn normalize_encoder(value: i32) -> f64 {
+fn normalize_enc(value: i32) -> f64 {
     const ENC_MIN: i32 = -128;
     const ENC_MAX: i32 = 128;
     let value = value.clamp(ENC_MIN, ENC_MAX);
     (value - ENC_MIN) as f64 / (ENC_MAX - ENC_MIN) as f64
+}
+
+fn normalize_pot(value: u16) -> f64 {
+    value as f64 / 1024.0
 }
