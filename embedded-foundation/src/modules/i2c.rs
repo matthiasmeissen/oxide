4406b12c489f74a32e84e1ad77105219fb5b1142
matthiasmeissen <meissenmatthias@gmail.com>
2025-10-04 17:30:47 +0200

    EMB - Add inputs to raspberry pi

--- embedded-foundation/src/modules/i2c.rs
+++ embedded-foundation/src/modules/i2c.rs
@@ -7,15 +7,20 @@ use std::thread;
 use std::time::Duration;
 use std::convert::TryInto;
 
-const STATE_SIZE: usize = 9;
+const STATE_SIZE: usize = 17;
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 struct DeviceState {
     button1: bool,
     button2: bool,
+    button3: bool,
+    button4: bool,
+    pot1_value: i16,
+    pot2_value: i16,
+    pot3_value: i16,
+    pot4_value: i16,
     encoder_button: bool,
     encoder_value: i32,
-    pot_value: i16,
 }
 
 pub fn start_i2c_thread(sender: Sender<Message>) {
@@ -35,16 +40,17 @@ pub fn start_i2c_thread(sender: Sender<Message>) {
                     let current_state = DeviceState {
                         button1: buffer[0] != 0,
                         button2: buffer[1] != 0,
-                        encoder_button: buffer[2] != 0,
-                        encoder_value: i32::from_le_bytes(buffer[3..7].try_into().unwrap_or_default()),
-                        pot_value: i16::from_le_bytes(buffer[7..9].try_into().unwrap_or_default()),
+                        button3: buffer[2] != 0,
+                        button4: buffer[3] != 0,
+                        pot1_value: i16::from_le_bytes(buffer[4..6].try_into().unwrap_or_default()),
+                        pot2_value: i16::from_le_bytes(buffer[6..8].try_into().unwrap_or_default()),
+                        pot3_value: i16::from_le_bytes(buffer[8..10].try_into().unwrap_or_default()),
+                        pot4_value: i16::from_le_bytes(buffer[10..12].try_into().unwrap_or_default()),
+                        encoder_button: buffer[12] != 0,
+                        encoder_value: i32::from_le_bytes(buffer[13..17].try_into().unwrap_or_default()),
                     };
 
                     if let Some(prev_state) = last_state {
-                        if current_state.encoder_value != prev_state.encoder_value {
-                            sender.send(Message::SetValue(0, normalize_enc(current_state.encoder_value))).unwrap();
-                        }
-
                         if current_state.button1 != prev_state.button1 {
                             let value = if current_state.button1 { 1.0 } else { 0.0 };
                             sender.send(Message::SetValue(4, value)).unwrap();
@@ -55,13 +61,39 @@ pub fn start_i2c_thread(sender: Sender<Message>) {
                             sender.send(Message::SetValue(5, value)).unwrap();
                         }
 
+                        if current_state.button3 != prev_state.button3 {
+                            let value = if current_state.button3 { 1.0 } else { 0.0 };
+                            sender.send(Message::SetValue(6, value)).unwrap();
+                        }
+
+                        if current_state.button4 != prev_state.button4 {
+                            let value = if current_state.button4 { 1.0 } else { 0.0 };
+                            sender.send(Message::SetValue(7, value)).unwrap();
+                        }
+
+                        if current_state.pot1_value != prev_state.pot1_value {
+                            sender.send(Message::SetValue(0, normalize_pot(current_state.pot1_value))).unwrap();
+                        }
+
+                        if current_state.pot2_value != prev_state.pot2_value {
+                            sender.send(Message::SetValue(1, normalize_pot(current_state.pot2_value))).unwrap();
+                        }
+
+                        if current_state.pot3_value != prev_state.pot3_value {
+                            sender.send(Message::SetValue(2, normalize_pot(current_state.pot3_value))).unwrap();
+                        }
+
+                        if current_state.pot4_value != prev_state.pot4_value {
+                            sender.send(Message::SetValue(3, normalize_pot(current_state.pot4_value))).unwrap();
+                        }
+
                         if current_state.encoder_button != prev_state.encoder_button {
                             let value = if current_state.encoder_button { 1.0 } else { 0.0 };
                             sender.send(Message::SetValue(6, value)).unwrap();
                         }
 
-                        if current_state.pot_value != prev_state.pot_value {
-                            sender.send(Message::SetValue(1, normalize_pot(current_state.pot_value))).unwrap();
+                        if current_state.encoder_value != prev_state.encoder_value {
+                            sender.send(Message::SetValue(0, normalize_enc(current_state.encoder_value))).unwrap();
                         }
                     }
 
