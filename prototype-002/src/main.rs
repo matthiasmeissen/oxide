70b2a635207022cd0404e701276dd808ef5cd809
matthiasmeissen <meissenmatthias@gmail.com>
2025-10-10 18:45:26 +0200

    PT2 - Add feature flags to conditional compile display simulator

--- prototype-002/src/main.rs
+++ prototype-002/src/main.rs
@@ -1,11 +1,7 @@
 
-// Add state and adjust to 32bit
-// Add coordinator
-// Add graphics and shaders
-// Add audio and dsp
-
-// Add midi
-// Add display and screens
+// Use cargo run --features "simulator"
+// And export LIBRARY_PATH="$LIBRARY_PATH:$(brew --prefix)/lib"
+// To run the display simulator
 
 pub mod state;
 pub mod coordinator;
@@ -14,9 +10,14 @@ pub mod audio;
 pub mod dsp;
 pub mod midi;
 
+#[cfg(feature = "simulator")]
+pub mod display_simulator;
+
+#[cfg(not(feature = "simulator"))]
 pub mod display;
-pub mod screens;
+#[cfg(not(feature = "simulator"))]
 pub mod i2c;
+pub mod screens;
 
 use state::*;
 use coordinator::*;
@@ -24,9 +25,12 @@ use graphics::*;
 use audio::*;
 use midi::*;
 
-#[cfg(target_os = "linux")]
+#[cfg(feature = "simulator")]
+use display_simulator::*;
+
+#[cfg(all(target_os = "linux", not(feature = "simulator")))]
 use display::*;
-#[cfg(target_os = "linux")]
+#[cfg(all(target_os = "linux", not(feature = "simulator")))]
 use i2c::*;
 
 use crossbeam_channel;
@@ -41,15 +45,24 @@ fn main() {
     let (audio_writer, audio_reader) = TripleBuffer::new(&State::default()).split();
 
     start_coordinator_thread(receiver, window_writer, display_writer, audio_writer);
-
     start_midi_thread(sender.clone());
-
-    #[cfg(target_os = "linux")]
-    start_display(display_reader);
-    #[cfg(target_os = "linux")]
-    start_i2c_thread(sender.clone());
-
     start_audio_thread(audio_reader);
 
-    start_graphics_thread(sender.clone(), window_reader);
+    #[cfg(feature = "simulator")]
+    {
+        println!("Running with display simulator");
+        start_display_simulator(display_reader);
+    }
+
+    #[cfg(not(feature = "simulator"))]
+    {
+        #[cfg(target_os = "linux")]
+        {
+            println!("Running on target hardware");
+            start_display(display_reader);
+            start_i2c_thread(sender.clone());
+        }
+    
+        start_graphics_thread(sender.clone(), window_reader);
+    }
 }
